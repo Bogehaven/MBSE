97bb8aceadc4f34560ccb22ffbab8c48deb29e77
Eric-66666 <114857381+Eric-66666@users.noreply.github.com>
2022-11-10 11:37:13 +0100

    Update TrafficManager.py

--- TrafficManager.py
+++ TrafficManager.py
@@ -17,6 +17,8 @@ class TrafficSimulator:
         self.max_y = self.data['y'].max()
         self.center_x = (self.max_x + self.min_x) / 2
         self.center_y = (self.max_y + self.min_y) / 2
+        # self.types = self.data['type'].unique()
+        # print(self.types)
 
         # Tweak to fit the projection
         self.resolution = 11132.0 * 2                           # 1 degree = 111.32km, works as zoom
@@ -34,13 +36,28 @@ class TrafficSimulator:
         x = self.center_x_offset + (vehicleX - self.center_x) * self.lat_coeff * self.resolution
         return (x, y)
 
+    def getVehicleColor(self,vehicle):
+        ColorMap={
+            'veh_petrol': (0,0,255),#blue
+            'veh_diesel':(30,144,255),#dodgerblue1
+            'veh_hybrid': (0,191,255),#deepskyblue1
+            'veh_ev':(135,206,235),#skyblue
+
+            'truck_petrol': (139,105,20),#goldenrod4
+            'truck_hybrid':(255,255,0)#yellow1
+        }
+        type=vehicle.type
+        Color=ColorMap[type]
+        return Color
+
     def draw(self, t, pollutionSurface):
         if (t >= len(self.timesteps)): return # No more data?
 
         vehicles = self.timesteps[t]
         for i, vehicle in vehicles.iterrows():
             pixel_x, pixel_y = self.getDegreeToPixelCoordinates(vehicle.x, vehicle.y)
-            pygame.draw.rect(pollutionSurface, (255, 255, 0), (pixel_x - 3, pixel_y - 3, 6, 6))
+            color = self.getVehicleColor(vehicle)
+            pygame.draw.rect(pollutionSurface, color, (pixel_x - 3, pixel_y - 3, 6, 6))
 
     def isSimulationDone(self):
         return self.simulationDone
